28fe45f167b128369319025b6332275bfb9d45cf
RaulRamirezMendoza <raulramirezmendoza@alumno.ieselrincon.es>
2024-02-08 19:34:55 +0000

    Validacion de formulario y encaso de no incluir imagen se le asgina una predeterminada

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -19,22 +19,39 @@ class AdminProductController extends Controller
     }
 
     public function store(Request $request){
-        $name = $request -> input('name');
-        $price = $request -> input('price');
-        $description = $request -> input('description');
-        
-        $uuid = Str::uuid()->toString();
-        $fileName= $uuid.".".($request -> file('image')->getClientOriginalExtension());
+        $validated = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'description' => 'required'
+        ]);
 
-        Storage::disk('public')->put(  
-            $fileName,
-            file_get_contents($request->file('image')->getRealPath())  
-        );
-        
-        Product::create(['nombre' => $name,'precio' => $price,'descripcion' => $description,'imagen' => $fileName]);
-        $viewData = [];
-        $viewData["title"] = "Admin Page - Products - Online Store";
-        $viewData["products"] = Product::all();
-        return view('admin.products')->with("viewData", $viewData);
+        if($validated){
+            $name = $request -> input('name');
+            $price = $request -> input('price');
+            $description = $request -> input('description');
+            
+            if($request->hasFile('image')){
+                $uuid = Str::uuid()->toString();
+                $fileName= $uuid.".".($request -> file('image')->getClientOriginalExtension());
+                Storage::disk('public')->put(  
+                    $fileName,
+                    file_get_contents($request->file('image')->getRealPath())  
+                );
+            }else{
+                $fileName='notYet.jpg';
+            }
+            
+            Product::create(['nombre' => $name,'precio' => $price,'descripcion' => $description,'imagen' => $fileName]);
+            $viewData = [];
+            $viewData["title"] = "Admin Page - Products - Online Store";
+            $viewData["products"] = Product::all();
+            return view('admin.products')->with("viewData", $viewData);
+
+        }else{
+            $viewData = [];
+            $viewData["title"] = "Admin Page - Products - Online Store";
+            $viewData["products"] = Product::all();
+            return view('admin.products')->with("viewData", $viewData);
+        }
     }
 }
